26900fb3fabf2770866159b834864d6d2a87dcb2
Mathyslol <93182170+mathyslolFN@users.noreply.github.com>
2022-10-29 20:00:04 +0200

    Update main.py

--- main.py
+++ main.py
@@ -6,7 +6,6 @@ import uvloop
 asyncio.set_event_loop_policy(uvloop.EventLoopPolicy())
 
 
-
 env_vars = {}
 if os.path.isfile('.device'):
     with open('.device') as f:
@@ -16,24 +15,24 @@ if os.path.isfile('.device'):
             key, value = line.strip().split('=', 1)
             env_vars[key] = value.replace("\"", "")
 else:
-  print("error device in the file")
+  print("error, are you sure you have put the ids correctly in the file .device ?")
   sys.exit()
 
-os.system('pip install -U XBXBOT')
+os.system('python3 -m pip install --upgrade XBXBOT')
 os.system('clear')
 
-import XBXBOT
+import XBXBOT2
 
 
 try:
-  client = XBXBOT.XBXBOT(
+  client = XBXBOT2.XBXBOT(
     device_id=env_vars['DEVICE_ID'],
     account_id=env_vars['ACCOUNT_ID'],
     secret=env_vars['SECRET']
 )
 
 except:
-  client = XBXBOT.XBXBOT(
+  client = XBXBOT2.XBXBOT(
     device_id=os.getenv('DEVICE_ID'),
     account_id=os.getenv('ACCOUNT_ID'),
     secret=os.getenv('SECRET')
@@ -44,4 +43,4 @@ try:
 except Exception as e:
   print(e)
   print("Can't login because your device auths is probably wrong.")
-  print("are you sure you have put yourr IDs in secrets env or in .device  file ?")
+  print("are you sure you have put your IDs in secrets env or in .device  file ?")
